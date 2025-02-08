29d295664bd3c189c250d484e9a2f6c34d4b6544
Siboniso <sibandzesiboniso0@gmail.com>
2025-02-08 16:27:11 +0200

    333

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-        <img src="../ki.png" alt="Image 1">
-        <img src="../icon.png" alt="Image 2">
-        <img src="../fnb.png" alt="Image 3">
+        <img src="../ki.png" alt="Image 1" width="45px">
+        <img src="../icon.png" alt="Image 2" width="45px">
+        <img src="../fnb.png" alt="Image 3" width="45px">
 </footer>
