6e8a2de9b9330cb2e8d38995b3038a7c6a1d95b6
Siboniso <sibandzesiboniso0@gmail.com>
2025-02-08 14:48:32 +0200

    login fix

--- mobile/appointments.php
+++ mobile/appointments.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login-page.php');
+    exit();
+}
 include '../zon.php';
 $conn = new Con();
 $db = $conn->connect();
