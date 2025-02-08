c06f6ea1b5adfc2a02a24a289ec3ceb09043e8fe
Siboniso <sibandzesiboniso0@gmail.com>
2025-02-08 15:30:28 +0200

    1

--- mobile/login-page.php
+++ mobile/login-page.php
@@ -97,7 +97,7 @@
             </div>
         <?php endif; ?>
         <div class="links">
-            <a href="mobile/forgot_login.php">Forgot Login?</a>
+            <a href="mobile/forgot_password.php">Forgot Login?</a>
             <a href="register.php">Register</a>
         </div>
         <div class="sponsors">
