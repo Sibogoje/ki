bf2b52bfba0236ae66b96d5a535ca2488882b5fb
Siboniso <sibandzesiboniso0@gmail.com>
2025-02-09 20:13:53 +0200

    Update footer image sources to use absolute URLs for improved accessibility

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark text-white text-center py-2">
-    <img src="fnb foundation.png" alt="Image 3" class="mx-2" style="width: 64px; height: 40px;">
-    <img src="icon.png" alt="Image 2" class="mx-2" style="width: 45px; height: 40px;">    
-    <img src="ki.png" alt="Image 1" class="mx-2" style="width: 45px; height: 40px;">
+    <img src="https://grinpath.com/fnb foundation.png" alt="Image 3" class="mx-2" style="width: 64px; height: 40px;">
+    <img src="https://grinpath.com/icon.png" alt="Image 2" class="mx-2" style="width: 45px; height: 40px;">    
+    <img src="https://grinpath.com/ki.png" alt="Image 1" class="mx-2" style="width: 45px; height: 40px;">
     </footer>
