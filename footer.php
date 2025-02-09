41fc5ff66d230178233641cef14e52bf702e85c2
Siboniso <sibandzesiboniso0@gmail.com>
2025-02-09 20:14:22 +0200

    Update footer background color to black for improved contrast

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-white text-center py-2">
+<footer class="bg-black text-white text-center py-2">
     <img src="https://grinpath.com/fnb foundation.png" alt="Image 3" class="mx-2" style="width: 64px; height: 40px;">
     <img src="https://grinpath.com/icon.png" alt="Image 2" class="mx-2" style="width: 45px; height: 40px;">    
     <img src="https://grinpath.com/ki.png" alt="Image 1" class="mx-2" style="width: 45px; height: 40px;">
