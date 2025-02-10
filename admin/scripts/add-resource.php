79794fd3b3da2c7afe463ffd1b0b6c218c1c64f8
Siboniso <sibandzesiboniso0@gmail.com>
2025-02-10 05:33:35 +0200

    Refactor file upload path to use local directory and universal URL variable

--- admin/scripts/add-resource.php
+++ admin/scripts/add-resource.php
@@ -15,10 +15,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $fileExtension = strtolower(end($fileNameCmps));
         
         // Set the target directory and file path
-        $uploadFileDir = $universal_url + 'admin/uploads/';
+        $uploadFileDir = '../uploads/';
         //$rr = 'scripts/';
+       // $universal_url
         $dest_path = $uploadFileDir . $fileName;
-        $dest_path1 = $uploadFileDir . $fileName;
+        $dest_path1 = $universal_url + $uploadFileDir . $fileName;
         
         // Move the file to the target directory
         if (move_uploaded_file($fileTmpPath, $dest_path)) {
